755fd9ff7b7125f0678813476d3cee9576767c82
Aymane-Guelbaoui <aymaneguelbaoui@gmail.com>
2025-07-19 22:13:35 +0100

    Create index.tsx

---
+++ alx-project-0x01/pages/users/index.tsx
@@ -0,0 +1,32 @@
+import UserCard from "@/components/common/UserCard";
+import Header from "@/components/layout/Header";
+import { UserProps } from "@/interfaces";
+
+const Users: React.FC<{ posts: UserProps[] }> = ({ posts }) => {
+  return (
+    <div className="flex flex-col h-screen">
+      <Header />
+      <main className="p-4">
+        <h1 className="text-2xl font-semibold mb-4">Users List</h1>
+        <div className="grid grid-cols-3 gap-4">
+          {posts.map((user: UserProps, index: number) => (
+            <UserCard {...user} key={index} />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const posts = await response.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+export default Users;
