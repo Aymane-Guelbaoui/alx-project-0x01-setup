93d247b6664f4a5f4b00e1c70e2ba31754659bc8
Aymane-Guelbaoui <aymaneguelbaoui@gmail.com>
2025-07-19 22:11:25 +0100

    Create index.tsx

---
+++ alx-project-0x01/pages/index.tsx
@@ -0,0 +1,9 @@
+const Home: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Welcome to our Application!</h1>
+    </div>
+  );
+};
+
+export default Home;
